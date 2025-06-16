454d79afeabc90fa20d1d45bc363ddf159d25d56
enock <>
2025-06-16 15:58:48 -0700

    saved one

---
+++ app/categories/page.js
@@ -0,0 +1,19 @@
+
+
+// import LoginForm from "@/components/ui/formLogin/form";
+// import Image from "next/image";
+// import Homes from "./home/page";
+
+import LoginForm from "@/components/ui/formLogin/form";
+import Homes from "./home/page";
+
+
+export default function Home() {
+  
+  return (
+    
+     <>
+  <LoginForm/>
+</>
+  );
+}
